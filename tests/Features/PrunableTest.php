c08a759aa87b3bb572388614529f7c79bda58192
freek <freek@spatie.be>
2021-07-25 21:08:15 +0200

    add prunable test

---
+++ tests/Features/PrunableTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Spatie\GitHubWebhooks\Models\GitHubWebhookCall;
+use function Spatie\PestPluginTestTime\testTime;
+
+it('will prune records after the configured amount of days', function() {
+    testTime()->freeze();
+
+    config()->set('github-webhooks.prune_webhook_calls_after_days', 5);
+
+    GitHubWebhookCall::create([
+        'name' => 'dummny name'
+    ]);
+
+    testTime()->addDays(5)->subSecond();
+    $this->artisan('model:prune');
+    expect(GitHubWebhookCall::count())->toBe(1);
+
+    testTime()->addSecond();
+    $this->artisan('model:prune', [
+        '--model' => [GitHubWebhookCall::class]
+    ]);
+    expect(GitHubWebhookCall::count())->toBe(0);
+});
